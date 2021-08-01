0c7c7e398a3b5e365a652b75d35d98766bfe7215
EnriqueIzel2 <eleaoizel@gmail.com>
2021-07-31 20:00:36 -0400

    refactor: rm unused component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
-import './App.css';
 import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
